df834841a422c188199a747266e5bf50ad02e3d9
puze8681 <rsef0601@naver.com>
2017-02-10 18:10:03 +0900

    friend list

--- app/src/main/java/com/example/parktaejun/chattingexample/PersonFragment.java
+++ app/src/main/java/com/example/parktaejun/chattingexample/PersonFragment.java
@@ -2,9 +2,15 @@ package com.example.parktaejun.chattingexample;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by parktaejun on 2017. 2. 10..
@@ -12,7 +18,55 @@ import android.view.ViewGroup;
 
 public class PersonFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle sabedInstanceState){
-        return inflater.inflate(R.layout.person_layout, container, false);
+    private EditText friend_edit;
+    private RecyclerView friend_list;
+    private FriendListAdapter listAdapter;
+    private List<User> items = new ArrayList<>();
+
+    public PersonFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.person_layout, container, false);
+        friend_edit = (EditText) view.findViewById(R.id.friend_edit);
+        friend_list = (RecyclerView) view.findViewById(R.id.friend_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        friend_list.setLayoutManager(layoutManager);
+
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+        items.add(new User("박태준"));
+        items.add(new User("윤영채"));
+
+        listAdapter = new FriendListAdapter(getContext(), items);
+        friend_list.setAdapter(listAdapter);
+
+        return view;
     }
 }
