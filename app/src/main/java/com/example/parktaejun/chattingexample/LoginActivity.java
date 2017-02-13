5060c65ce655680322e42fb5adf3c741b2c62db4
puze8681 <rsef0601@naver.com>
2017-02-13 14:44:28 +0900

    facebook login

--- app/src/main/java/com/example/parktaejun/chattingexample/LoginActivity.java
+++ app/src/main/java/com/example/parktaejun/chattingexample/LoginActivity.java
@@ -1,11 +1,11 @@
 package com.example.parktaejun.chattingexample;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +14,18 @@ import android.widget.EditText;
 import com.example.parktaejun.chattingexample.Datas.IDCard;
 import com.example.parktaejun.chattingexample.Server.IDCardService;
 import com.example.parktaejun.chattingexample.Server.Util;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import retrofit2.Response;
 
@@ -25,8 +35,9 @@ import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
     private AlertDialog dialog;
-    Button loginbtn;
 
+    Button loginbtn;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +54,51 @@ public class LoginActivity extends AppCompatActivity {
                 new LoginAsynTask().execute(id.getText().toString(), pass.getText().toString());
             }
         });
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.i("fuck", "User ID: " + loginResult.getAccessToken().getUserId());
+                Log.i("fuck", "Auth Token: " + loginResult.getAccessToken().getToken());
+
+                String userID = loginResult.getAccessToken().getUserId();
+                String userToken = loginResult.getAccessToken().getToken();
+
+                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                mainIntent.putExtra("userID", userID);
+                mainIntent.putExtra("userToken", userToken);
+                startActivity(mainIntent);
+            }
+
+            @Override
+            public void onCancel()
+            {
+                // App code
+                Log.w("fuck", "Cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception)
+            {
+                // App code
+                Log.e("fuck", "Error", exception);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public class LoginAsynTask extends AsyncTask<String, Void, Void> {
@@ -73,5 +129,4 @@ public class LoginActivity extends AppCompatActivity {
         super.onDestroy();
         dialog.dismiss();
     }
-
 }
